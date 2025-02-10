3a805230ec835b3e5c021d3ca589d1ad46c5b638
kirillkoncha <majortomblog@gmail.com>
2025-02-10 18:24:39 +0100

    changed pid extraction

--- src/features_extraction/constants.py
+++ src/features_extraction/constants.py
@@ -1,16 +1,17 @@
 PID_DEPRELS = frozenset(
-    (
+    [
         "advcl",
         "advmod",
         "amod",
         "appos",
-        # "cc", is exlcuded in the paper
+        # "cc",
         "csubj",
         "csubjpass",
         "det",
         "neg",
         "npadvmod",
         "nsubj",
+        "nsubjpass",
         "nummod",
         "poss",
         "predet",
@@ -19,13 +20,13 @@ PID_DEPRELS = frozenset(
         "quantmod",
         "tmod",
         "vmod",
-    )
+    ]
 )
 
 SID_NSUBJ_NO = frozenset(("it", "this"))
 DET_NO = frozenset(("a", "an", "the"))
 
-PATH_TO_LOW_SPECIFICITY = "/Users/kirillkonca/Documents/dementia_prediction/sentences_specificity_filtered.txt"
+PATH_TO_LOW_SPECIFICITY = "/Users/kirillkonca/Documents/MAThesis/dementia_prediction/sentences_specificity_filtered.txt"
 
 with open(PATH_TO_LOW_SPECIFICITY, "r", encoding="utf-8") as file:
     sentences = [line.strip() for line in file]
--- src/features_extraction/features_extractor.py
+++ src/features_extraction/features_extractor.py
@@ -1,11 +1,13 @@
 import conllu
 
+from pycpidr import depid
+
 from src.features_extraction.constants import (DET_NO,
                                                LOW_SPECIFICITY_SENTENCES,
                                                PID_DEPRELS, SID_NSUBJ_NO)
 
 
-class FeatureExtractor:
+class FeaturesExtractor:
     def __init__(self):
         self.pid_deprels = PID_DEPRELS
 
@@ -13,46 +15,35 @@ class FeatureExtractor:
         self.det_no = DET_NO
         self.low_specificity_sentences = LOW_SPECIFICITY_SENTENCES
 
+    def extract_tree_depth(self, text: str) -> float:
+        text = conllu.parse(text)
+        depth = 0
+
+        for sentence in text:
+            depth += self._extract_one_tree_depth(sentence)
+
+        return depth/len(text)
+
     def extract_pid(self, text: str) -> float:
         """
         Counts Propositional Idea Density according to Sirts et al. (2017)
 
         Args:
-            text (conllu.models.SentenceList): Token list of a sentence
+            text (str): Text speech
 
         Returns:
             float: Propositional Idea Density normalised by token count
         """
-        sid_weighted_deprels = []
-        token_counter = 0
         text = conllu.parse(text)
-
+        score = 0
         for sentence in text:
-            token_counter += self._get_sentence_length(sentence)
             if not self._filter_sentence_nsubj(sentence):
                 continue
             if not self._filter_sentence_specifity(sentence):
                 continue
-            for token in sentence:
-                if token["deprel"] == "punct":
-                    continue
-                if token["deprel"] == "det" and token["lemma"].lower() in self.det_no:
-                    continue
-                if (
-                    token["deprel"] == "nsubj"
-                    and token["lemma"].lower() in self.sid_nsubj_no
-                ):
-                    continue
-                if token["deprel"] not in self.pid_deprels:
-                    continue
-
-                head_lemma = sentence[token["head"] - 1]["lemma"].lower()
-                deprel_str = f"{token['deprel']}({token['lemma'].lower()},{head_lemma})"
-
-                if deprel_str not in sid_weighted_deprels:
-                    sid_weighted_deprels.append(deprel_str)
-
-        return len(sid_weighted_deprels) / token_counter
+            pid, _, _ = depid(sentence.metadata["text"], is_depid_r=True)
+            score += pid
+        return score / len(text)
 
     def _get_sentence_length(self, sentence: conllu.models.TokenList) -> int:
         """
@@ -107,7 +98,7 @@ class FeatureExtractor:
             return False
         return True
 
-    def extract_tree_depth(sentence: conllu.models.TokenList) -> int:
+    def _extract_one_tree_depth(self, sentence: conllu.models.TokenList) -> int:
         """
         Compute the tree depth of a given sentence. From Taktasheva et al. (2024)
 
